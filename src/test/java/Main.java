e058ef8b3a86aea6025542833a32769e3731fcdc
MhmdSait <mehmedsaidtasdemir@gmail.com>
2023-01-12 19:00:44 +0300

    first commit

---
+++ src/test/java/Main.java
@@ -0,0 +1,77 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class Main {                       //12. Ders
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/text-box"); //14.Ders
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement fullName = driver.findElement(By.id("userName"));
+        fullName.click();
+        fullName.sendKeys("qwerty");
+
+        WebElement eMail = driver.findElement(By.cssSelector(".mr-sm-2[placeholder=\"name@example.com\"]"));
+        eMail.click();
+        eMail.sendKeys("asdf@gmail.com");
+
+        WebElement currentAdress = driver.findElement(By.cssSelector(".form-control[placeholder=\"Current Address\"]"));
+        currentAdress.click();
+        currentAdress.sendKeys("Bursa");
+
+        JavascriptExecutor jsx = (JavascriptExecutor) driver; //selenium sf. aş. kaydırma
+        jsx.executeScript("window.scrollBy(0,350)", "");
+
+        WebElement permanentAdress = driver.findElement(By.xpath("//textarea[@id=\"permanentAddress\"]"));
+        permanentAdress.click();
+        permanentAdress.sendKeys("Bursa,Türkiye");
+
+        WebElement submitbutton = driver.findElement(By.id("submit"));
+        submitbutton.click();
+
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+       driver.quit();
+
+        /*
+
+        //13.ders//Selenium WebDriver Komutları
+
+        //Timeouts//
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS); //Sayfanın yüklenmesini bekler
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS); //Sayfa üzerinde yapılan işlemi
+                                                                             // bekler
+        // Window
+        driver.manage().window().maximize(); //pen. max. eder
+        int X = driver.manage().window().getPosition().getX(); //Pencerenin X düzlemindeki konumunu verir.
+        int Y = driver.manage().window().getPosition().getY(); //Pencerenin Y düzlemindeki konumunu verir.
+
+        //Url
+        driver.get("https:url"); //Sit. git. amaçlı
+        String CurrentUrl = driver.getCurrentUrl(); // Sayfadaki erl'yi getirir.
+        String Title = driver.getTitle(); // Sf. baş. get.
+        String PageSource = driver.getPageSource(); // Sf'nin kaynak kodlarini getirir.
+
+        */
+    }
+
+}
---
+++ src/test/java/Main2.java
@@ -0,0 +1,78 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+
+public class Main2 { // 16.Ders Proje-1
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://demoqa.com/");
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+
+        JavascriptExecutor jsx = (JavascriptExecutor) driver;
+        jsx.executeScript("window.scrollBy(0,300)", "");
+
+        WebElement elements = driver.findElement(By.className("card-body"));
+        elements.click();
+
+        WebElement webTables = driver.findElement(By.xpath("//span[normalize-space()='Web Tables']"));
+        webTables.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement addButton = driver.findElement(By.id("addNewRecordButton"));
+        addButton.click();
+
+        WebElement firstName = driver.findElement(By.id("firstName"));
+        firstName.click();
+        firstName.sendKeys("ilkKan");
+
+        WebElement lastName = driver.findElement(By.id("lastName"));
+        lastName.click();
+        lastName.sendKeys("davasi");
+
+        WebElement eMail = driver.findElement(By.cssSelector("#userEmail"));
+        eMail.click();
+        eMail.sendKeys("ilk@gmail.com");
+
+        WebElement age = driver.findElement(By.id("age"));
+        age.click();
+        age.sendKeys("04");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        WebElement salary = driver.findElement(By.id("salary"));
+        salary.click();
+        salary.sendKeys("13000");
+
+        WebElement department = driver.findElement(By.xpath("//input[@id='department']"));
+        department.click();
+        department.sendKeys("Software Test Specialist");
+
+        WebElement submitButton = driver.findElement(By.cssSelector("#submit"));
+        submitButton.click();
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        driver.quit();
+    }
+}
